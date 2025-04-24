ca493e220964308a1f124b1bb32fe8b342e4ff68
kodongwon <dw9706@gmail.com>
2025-04-24 13:46:08 +0900

    feat: 필수 기능 Lv 3. 클래스 생성
    - Main, Kiosk, MenuItem 생성.

---
+++ src/com/dongwon/kiosk/required/lv3/Kiosk.java
@@ -0,0 +1,5 @@
+package com.dongwon.kiosk.required.lv3;
+
+public class Kiosk {
+
+}
---
+++ src/com/dongwon/kiosk/required/lv3/Main.java
@@ -0,0 +1,7 @@
+package com.dongwon.kiosk.required.lv3;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
