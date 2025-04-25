871e245733519e5d398274f5ae1edf48c6d3fd4d
kodongwon <dw9706@gmail.com>
2025-04-25 10:31:04 +0900

    feat: 도전 기능 Lv 1. 장바구니 관리용 Cart 생성
    - cartItem : List에 MenuItem 보관.
    - addItem : cartItem에 MenuItem 추가.
    - getTotalPrice : 장바구니 전체 가격 합산
    - isEmpty : 장바구니 비어있는지 확인

---
+++ src/com/dongwon/kiosk/challenge/lv1/Cart.java
@@ -0,0 +1,25 @@
+package com.dongwon.kiosk.challenge.lv1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+    private final List<MenuItem> cartItems = new ArrayList<>();
+
+    public void addItem(MenuItem item) {
+        cartItems.add(item);
+    }
+
+    public double getTotalPrice() {
+        double total = 0.0;
+        for (MenuItem cartItem : cartItems) {
+            total += cartItem.getPrice();
+        }
+        return total;
+    }
+
+    public boolean isEmpty() {
+        return cartItems.isEmpty();
+    }
+
+}
